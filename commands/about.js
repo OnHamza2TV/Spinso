f7364d2dd3383a9be115430cfcd7b56d0f3bf078
OnHamzaYT <70980195+OnHamzaYT@users.noreply.github.com>
2020-11-18 16:07:13 +0000

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 module.exports = {
     run: message => {
     message.channel.send(new Discord.MessageEmbed()
-        .setTitle("<:info:774066090111664168>  Informations sur " + `Spinso `)
+        .setTitle("<:info:774066090111664168>  Informations sur " + `Spinso\n  ● Merci à Flaticon pour presque tous les emojis!`)
         .addField('<:dev:774065913003638814>  __**Développeur :**__', `On Hamza#9999`)
         .addField("📂 __Version de discord.js__ :", `v12.2.0`)
         .addField("__**Lien d'invitation :**__", "[**Cliquez ici**](https://discordapp.com/oauth2/authorize?client_id=768950260163674162&scope=bot&permissions=8)", true)
