71d795d91ba9e931abb82553fa313d09c5d0f5fc
OnHamzaYT <70980195+OnHamzaYT@users.noreply.github.com>
2020-11-18 19:14:25 +0100

    Create createticket.js

---
+++ commands/createticket.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports = {
+    run: async (message, args, functions) => {
+    if(!message.member.hasPermission("ADMINISTRATOR"))
+    {
+        return message.channel.send("> :no_entry: Désolé tu n'as pas les permissions ``ADMINISTRATOR`` :no_entry:");
+    }
+
+if(message && message.deletable) message.delete().catch(e => {});
+
+let embed = new Discord.MessageEmbed()
+.setTitle(`Système de Ticket`)
+.setFooter(`Spinso | Crée par On Hamza.`, bot.user.displayAvatarURL())
+.setDescription(`Réagissez avec 🎟️ pour créer un ticket.`);
+message.channel.send(embed).then(m => {
+  m.react('🎟️');
+});
+
+},
+    name: 'createticket',
+    guildOnly: true
+}
