e9b301d2708f445def9d9501b4e36e06cb698a57
OnHamzaYT <70980195+OnHamzaYT@users.noreply.github.com>
2020-11-19 11:52:50 +0000

    Update events.js

--- functions/events.js
+++ functions/events.js
@@ -2,15 +2,15 @@ const { readdirSync } = require("fs");
 const { join } = require("path");
 const eventDir = join(__dirname, "..", "events");
 
-module.exports.run = (bot) => {
+module.exports.run = (client) => {
     const eventFiles = readdirSync(eventDir);
 
     for(const eventFile of eventFiles) {
         const event = require(`${eventDir}/${eventFile}`);
         const eventName = eventFile.split(".").shift();
-        bot.on(eventName, event.bind(null, bot));
+        client.on(eventName, event.bind(null, client));
         delete require.cache[require.resolve(`${eventDir}/${eventFile}`)];
     }
-    bot.events = eventFiles.length;
+    client.events = eventFiles.length;
     console.log(`Loaded ${eventFiles.length} events !`);
 }
