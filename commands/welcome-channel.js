bab5cb21819b788908a1a3c01a822fa3ffb26f87
OnHamzaYT <70980195+OnHamzaYT@users.noreply.github.com>
2020-11-19 19:45:31 +0000

    Update welcome-channel.js

--- commands/welcome-channel.js
+++ commands/welcome-channel.js
@@ -1,24 +1,28 @@
 const db = require('quick.db')
+const jimp = require('jimp');
 
 module.exports = {
-    run: async (message, member, args) => { 
-        if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.channel.send(`Vous n'avez pas la permission d'utiliser cette commande!`)
-    if(!args[0]) return message.channel.send(message.guild.channels.cache.get(db.get(`welcomechannel_${message.guild.id}`)) ? `Le salon de bienvenue est ${message.guild.channels.cache.get(db.get(`welcomechannel_${message.guild.id}`)).name} !` : "Le salon de bienvenue n'a pas été encore configuré!")
-    let MentionedChannel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
-    if(!MentionedChannel){
-        if(args[0] == "reset") {
-            if(!db.get(`welcomechannel_${message.guild.id}`)) return message.channel.send("Le salon de bienvenue n'a pas été configuré!")
-            db.set(`welcomechannel_${message.guild.id}`, null)
-            return message.channel.send("Le système de bienvenue a bien été reset!")
-        } else {
-            return message.channel.send("Veuillez mentionner ou donner l'ID un salon qui sera le nouveau salon de bienvenue du serveur!")
-        }
-    }
-    if(MentionedChannel.id == db.get(`welcomechannel_${message.guild.id}`)) return message.channel.send("Ce salon est déjà configuré en tant que salon de bienvenue!")
-    if(MentionedChannel.type !== "text") return message.channel.send("Ce salon n'est pas un salon textuel!")
-    db.set(`welcomechannel_${message.guild.id}`, MentionedChannel.id)
-    message.channel.send(`Le nouveau salon de bienvenue sera ${MentionedChannel}!`)
+    run: async (message, args) => { 
+	let permission = message.member.hasPermission("ADMINISTRATOR");
 
+if(!permission) return message.channel.send("You are missing the permission `ADMINISTRATOR`")
+
+ let cArgs = args[0]
+ 
+ if(isNaN(cArgs)) return message.channel.send("You must specify a valid id for the welcome channel!")
+	 
+ try{
+	 client.guilds.get(message.guild.id).channels.get(cArgs).send("Welcome channel set!")
+	 
+ db.set(`welcomechannel_${message.guild.id}`, ID)
+ 
+ message.channel.send("You have successfully set the welcome channel to <#" + cArgs + ">")
+return;
+ }catch(e){
+	return message.channel.send("Error: missing permissions or channel doesn't exist")
+ }
+ 
+ 
 },
 name: 'welcome-channel',
 guildOnly: true
