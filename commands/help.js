644234172fb81a5260f463db45acaea6532de11d
OnHamzaYT <70980195+OnHamzaYT@users.noreply.github.com>
2020-11-19 20:57:12 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports = {
     run: message => {
 const help = new Discord.MessageEmbed()
             .setTitle(`● <:delta_help:774824627351322627> Page d'aide - Spinso`)
-            .setDescription("● :exclamation: Le bot n'est pas encore fini, il y a quelques commandes qui ne marchent pas encore.\n ● Mon préfix est `.`\n ● Je possède un total de `63` commandes")
+            .setDescription("● :exclamation: Le bot n'est pas encore fini, il y a quelques commandes qui ne marchent pas encore.\n ● Mon préfix est `.`\n ● Je possède un total de `67` commandes")
             .addField("<:general:774824803186114560> __Général__", "`help`, `invite`, `embed`,  `img`,  `ui`, `serv-info`, `sondage`, `about`,`ascii`, `reversetext`, `pp`, `rdm-number`, `invite`")
             .addField("<:moderation:774826227184828437> __Modération__", "`ban`, `kick`, `tempban`,`clear`, `unban`, `mute`, `unmute`, `tempmute`, `slowmode`")
             .addField("<:fun:774824901877956640> __Divertissement__ ", "`anonymous`, `fbi`, `poutine`, `trump`, `macron`, `cheh`, `meme`, `coinflip`, `issou`, `covid`, `8ball`, `nitro`, `panda`, `koala`, `fox`, `cat`, `dog`, `captcha`,`hastbin`, `weather`, `createinvite`, `covid-stats`")
