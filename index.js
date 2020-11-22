6499c9bd624297d852e8bef65336296c574e552f
OnHamzaYT <70980195+OnHamzaYT@users.noreply.github.com>
2020-11-22 10:24:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -181,15 +181,40 @@ client.on('message', function (message) {
 const { MessageEmbed } = require('discord.js');
 
       client.on('guildCreate', guild => {
-    const embed = new MessageEmbed()
-    	.setTitle("`➕` Spinso a rejoin un serveur.")
-        .setDescription(`Merci a ${guild.name}`)
-        .addField("📋 __Nom du serveur__", guild.name)
-        .addField("👤 __Propriétaire__ :", guild.ownerID)
-        .addField("🌍 __Région du serveur__ :", guild.region)
-        .addField("📝 __ID du serveur__ :", guild.id)
-    	.addField("📊 __Nombre des membres__ :", guild.memberCount)
-        .setColor("#F03A17")
+    let region = {
+        "brazil": `:flag_br: \`Brésil\``,
+        "southafrica": `:flag_za: \`Afrique du Sud\``,
+        "eu-central": `:flag_eu: \`Europe Central\``,
+        "europe": `:flag_eu: \`Europe\``,
+        "russia": `:flag_ru: \`Russie\``,
+        "singapore": `:flag_sg: \`Singapour\``,
+        "us-central": `:flag_us: \`États-Unis Central\``,
+        "sydney": `:flag_au: \`Sydney\``,
+        "japan": `:flag_jp: \`Japon\``,
+        "us-east": `:flag_us: \`Est des États-Unis\``,
+        "us-south": `:flag_us: \`Sud des États-Unis\``,
+        "us-west": `:flag_us: \`Ouest des États-Unis\``,
+        "eu-west": `:flag_eu: \`Europe de l'Ouest\``,
+        "vip-us-east": `:flag_us: \`VIP U.S. East ?\``,
+        "london": `:flag_gb: \`Londres\``,
+        "india": `:flag_in: \`Inde\``,
+        "amsterdam": `:flag_nl: \`Amsterdam\``,
+        "hongkong": `:flag_hk: \`Hong Kong\``
+    };
+
+    const embed = new Discord.MessageEmbed()
+        .setColor(`GREEN`)
+        .setAuthor(`${client.user.username} à rejoind le serveur`, client.user.displayAvatarURL({
+            dynamic: true
+        }))
+        .setTitle(`Informations du serveur`)
+        .addField(`📰 • __Nom__`, `\`${guild.name}\``, true)
+        .addField(`📰 • __Région__`, `${region[guild.region]}`, true)
+        .addField(`📰 • __Rôles__`, `\`${guild.roles.cache.size}\``, true)
+        .addField(`📰 • __Membres__`, `\`${guild.memberCount - guild.members.cache.filter(m => m.user.bot).size}\``, true)
+        .addField(`📰 • __ID__`, `\`${guild.id}\``, true)
+        .addField(`👑 • __Propriétaire__`, `${guild.owner || `\`${guild.owner.user.username}\``}`, true)
+        .setTimestamp()
       client.channels.cache.get('778345249017298955').send(embed);
 });
 
